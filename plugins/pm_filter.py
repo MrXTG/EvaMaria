a1d429ba10a5f420d7f3417eba4d6fcb88e3ad11
Mr.X <69637965+MrXTG@users.noreply.github.com>
2022-03-31 21:53:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -677,7 +677,7 @@ async def auto_filter(client, msg, spoll=False):
     TEMPLATE = settings['template']
     if imdb:
         cap = TEMPLATE.format(
-            query=search,
+            🔥query=search,
             title=imdb['title'],
             votes=imdb['votes'],
             aka=imdb["aka"],
