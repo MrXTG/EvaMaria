fe0a44c14da48faaa78a7c65f419581dfc57e27c
MrZidxx <102791778+MrZidxx@users.noreply.github.com>
2022-04-01 22:01:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -652,11 +652,11 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🪄{file.file_name}",
+                    text=f"✨️ [{get_size(file.file_size)}]",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"✨️ {file.file_name}",
                     callback_data=f'{pre}_#{file.file_id}',
                 ),
             ]
